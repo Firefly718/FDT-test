adde4b688f2e264d55b0f7cdbba74f95f4cb406b
Bohdan <web.dev.firefly@gmail.com>
2021-02-05 13:01:17 +0300

    finished comments for main page

---
+++ default-js/script.js
@@ -0,0 +1,9 @@
+// slider
+$(document).ready(function(){
+  $('.comments__slider').slick({
+    dots: false,
+    arrows: true,
+    prevArrow: '<div class="comments__slider--left"></div>',
+    nextArrow: '<div class="comments__slider--right"></div>'
+  });
+});
