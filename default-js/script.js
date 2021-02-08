a892d31d56f55093f9d09c6e23685926844c4155
Bohdan <web.dev.firefly@gmail.com>
2021-02-08 18:40:31 +0300

    finished banner for third page

--- default-js/script.js
+++ default-js/script.js
@@ -7,3 +7,12 @@ $(document).ready(function(){
     nextArrow: '<div class="comments__slider--right"></div>'
   });
 });
+
+$(document).ready(function(){
+  $('.banner-third__slider').slick({
+    dots: true,
+    arrows: false,
+    prevArrow: '<div class="comments__slider--left"></div>',
+    nextArrow: '<div class="comments__slider--right"></div>'
+  });
+});
