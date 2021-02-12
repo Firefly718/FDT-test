6a8252e6c8b264738a2c1d10ea4bbea8cb11e301
Bohdan <web.dev.firefly@gmail.com>
2021-02-12 12:52:06 +0300

    fixed icons, nav elements and slider

--- default-js/script.js
+++ default-js/script.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
   $('.comments__slider').slick({
     dots: false,
     arrows: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
     prevArrow: '<div class="comments__slider--left"></div>',
     nextArrow: '<div class="comments__slider--right"></div>'
   });
@@ -12,6 +14,8 @@ $(document).ready(function(){
   $('.banner-third__slider').slick({
     dots: true,
     arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
     prevArrow: '<div class="comments__slider--left"></div>',
     nextArrow: '<div class="comments__slider--right"></div>'
   });
