ed4d83e6bee7a242200a42aed13ae05cd42ba275
Bohdan <web.dev.firefly@gmail.com>
2021-02-13 14:07:43 +0300

    finished responsive for header main page

--- default-js/script.js
+++ default-js/script.js
@@ -1,4 +1,4 @@
-// slider
+// slider (first page)
 $(document).ready(function(){
   $('.comments__slider').slick({
     dots: false,
@@ -10,6 +10,7 @@ $(document).ready(function(){
   });
 });
 
+// slider (third page)
 $(document).ready(function(){
   $('.banner-third__slider').slick({
     dots: true,
@@ -18,5 +19,15 @@ $(document).ready(function(){
     autoplaySpeed: 2000,
     prevArrow: '<div class="comments__slider--left"></div>',
     nextArrow: '<div class="comments__slider--right"></div>'
+  });
+});
+
+// burger
+$(document).ready(function(){
+  // $("body > header > div > div > div > div.header__nav").hide();
+
+  $("body > header > div > div > div > div.burger").click(function(){
+    $("body > header > div > div > div > div.header__nav").toggle('slow', 'swing');
+    $("body > header > div > div > div > div.header__nav").css('display', 'flex');
   });
 });
